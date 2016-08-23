071383c95da90b6bb10ec04b1dcb24fab838fc51
fedor <fedor.malyshkin@yandex.ru>
2016-08-24 00:22:22 +0300

    added building elasticsearch container and runnning all containers under non-priveledged user

--- deployment/templates/elasticsearch/elasticsearch.pp
+++ deployment/templates/elasticsearch/elasticsearch.pp
@@ -1,34 +1,34 @@
-user { 'mongo':
+user { 'elastic':
 	ensure => "present",
 }
 
 # working dir
-file { '/mongodb':
+file { '/elasticsearch':
 	ensure => "directory",
 	recurse => "true",
-	owner => "mongo",
-	group=> "mongo",
+	owner => "elastic",
+	group=> "elastic",
 	mode=>"ug=rwx,o=rx",
 }
 
 file { '/data':
 	ensure => "directory",
 	recurse => "true",
-	owner => "mongo",
-	group=> "mongo",
+	owner => "elastic",
+	group=> "elastic",
 	mode=>"ug=rwx,o=rx",
 }
 file { '/data/db':
 	ensure => "directory",
 	recurse => "true",
-	owner => "mongo",
-	group=> "mongo",
+	owner => "elastic",
+	group=> "elastic",
 	mode=>"ug=rwx,o=rx",
 }
 file { '/data/logs':
 	ensure => "directory",
 	recurse => "true",
-	owner => "mongo",
-	group=> "mongo",
+	owner => "elastic",
+	group=> "elastic",
 	mode=>"ug=rwx,o=rx",
 }
